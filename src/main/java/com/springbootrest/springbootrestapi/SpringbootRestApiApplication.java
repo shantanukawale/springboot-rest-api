3a4457e06d78291f89af8c487c152d4479dd7c05
Shantanu Kawale <shantanukawale@gmail.com>
2021-06-27 14:50:25 +0530

    feat(logs): add logs

--- src/main/java/com/springbootrest/springbootrestapi/SpringbootRestApiApplication.java
+++ src/main/java/com/springbootrest/springbootrestapi/SpringbootRestApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootRestApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootRestApiApplication.class, args);
 	}
--- src/main/java/com/springbootrest/springbootrestapi/controller/UserController.java
+++ src/main/java/com/springbootrest/springbootrestapi/controller/UserController.java
@@ -7,6 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.NoSuchElementException;
 import java.util.List;
 
@@ -15,9 +18,11 @@ import java.util.List;
 public class UserController {
 	@Autowired
 	UserService userService;
+	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
 	@GetMapping("")
 	public List<User> list() {
+		logger.info("test log");
 		return userService.listAllUser();
 	}
 
